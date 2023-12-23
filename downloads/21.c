c0acc6adf82fa3bc221d1e57d20af4ba9abbee28
41223201 <144991783+41223201@users.noreply.github.com>
2023-12-23 08:55:35 +0000

    上傳  C_ex  練習二 10個練習，及修改頁面大小標題

--- downloads/21.c
+++ downloads/21.c
@@ -1,13 +1,8 @@
 #include<stdio.h>
-void main()
-{
-int a,b;
-a = 15;
-b = 1;
-printf("%d \n", a | b ); /* a OR b */
-printf("%d \n", a & b ); /* a AND b */
-printf("%d \n", a ^ b ); /* a XOR b */
-printf("%d \n", a << 1 ); /* a 位元左移 1 位 */
-printf("%d \n", a >> 1 ); /* a 位元右移一位 */
-printf("%d \n", ~a ); /* A 的補數運算 */
+
+void main() {
+    int i;
+    for (i = 0; i < 6; i++) {
+        printf("%d\n", i); /* 印出 i */
+    }
 }
