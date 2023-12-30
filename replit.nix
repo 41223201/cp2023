8e1158368f8695ee2907858b95227633ea4b01f3
41223201 <144991783+41223201@users.noreply.github.com>
2023-12-30 08:26:26 +0000

    增加w16

--- replit.nix
+++ replit.nix
@@ -1,10 +1,12 @@
 { pkgs }: {
-    deps = [
-      pkgs.libev
-      pkgs.gnuplot
-      pkgs.ncurses.dev
-      pkgs.gd
-    ];
+  deps = [
+    pkgs.imagemagick
+    pkgs.libev
+    pkgs.gnuplot
+    pkgs.ncurses.dev
+    pkgs.gd
+    pkgs.freetype
+  ];
   env = {
     PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
       pkgs.libev
